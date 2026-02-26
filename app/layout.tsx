4e79639501eb46d4abc6533abb5bc4aef5f5b5b5
Sareena Sandeep <242ta31135@ashokacollege.in>
2026-02-26 23:26:54 +0530

    favicon file changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,8 @@ export const metadata: Metadata = {
   title: "ARRAY",
   description: "From chat noise to traceable decisions",
   icons: {
-    icon: [
-      { url: "/cube.png", type: "image/png", sizes: "any" },
-    ],
-    shortcut: [
-      { url: "/cube.png", type: "image/png" },
-    ],
+    icon: "/cube.ico",
+    shortcut: "/cube.ico",
   },
 };
 
@@ -32,11 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Explicit type attribute required for Firefox to recognise the favicon */}
-        <link rel="icon" type="image/png" href="/cube.png" />
-        <link rel="shortcut icon" type="image/png" href="/cube.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
